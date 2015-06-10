23cd20ecb707945fddda1aebf805ddda4961042e
Jakub Sikora <jakub.sikora.en@gmail.com>
2015-06-10 21:17:11 +0100

    placeholder

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Blue Nile - Travel and Services Ltd.</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+	<!--link rel="stylesheet/less" href="less/bootstrap.less" type="text/css" /-->
+	<!--link rel="stylesheet/less" href="less/responsive.less" type="text/css" /-->
+	<!--script src="js/less-1.3.3.min.js"></script-->
+	<!--append ‘#!watch’ to the browser URL, then refresh the page. -->
+
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/bluenile.css" rel="stylesheet">
+
+  <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+  <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+  <![endif]-->
+
+  <!-- Fav and touch icons -->
+  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="img/apple-touch-icon-144-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="img/apple-touch-icon-114-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/apple-touch-icon-72-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" href="img/apple-touch-icon-57-precomposed.png">
+
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/scripts.js"></script>
+</head>
+
+<body>
+<div class="container">
+	<div class="row clearfix">
+		<div class="col-md-12 column logo-wrapper">
+            <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+        <div class="navbar-header">
+           <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+           <span class="sr-only">Toggle navigation</span>
+           <span class="icon-bar"></span>
+           <span class="icon-bar"></span>
+           <span class="icon-bar"></span>
+           </button>
+           <img src="/img/iata.png" />
+           <img src="/img/atol.png" />
+        </div>
+
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav navbar-right">
+            <li class='header-item'>
+              <span class="glyphicon glyphicon-earphone"></span> 020 7723 5050
+            </li>
+            <li>
+              <a href="mailto:sales@blueniletravel.co.uk"><span class="glyphicon glyphicon-envelope"></span> Email</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+			<img src="/img/logo.png" />
+      <h3>Coming soon...</h3>
+		</div>
+	</div>
+  <div class="row clearfix">
+    <div class="col-md-12 column banner-wrapper">
+      <img src="/img/banner.png" width=600 />
+    </div>
+  </div>
+  <div class="row clearfix">
+    <div class="col-md-12 column airlogos-wrapper">
+      <img src="/img/airlogos.png" width=600 />
+    </div>
+  </div>
+  <div class="row clearfix">
+    <div class="col-md-12 column footer-wrapper">
+      31 Crawford Place, London W1H 4LL
+    </div>
+  </div>
+</div>
+</body>
+</html>
